279ff2b7149acd0406f7f576cb9f3573b0590997
Estela Sangkop <estelasangkop@gmail.com>
2021-06-27 18:47:58 +1200

    Add app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    api_key=os.environ['GOOGLE_CALENDAR_API_KEY']
+    return render_template("index.html", api_key=api_key)
+
+if __name__ == '__main__':
+    app.run(debug=True)
