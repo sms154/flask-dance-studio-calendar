d3051485ce347f494545df70d9f112fcc498542b
sms154 <68546983+sms154@users.noreply.github.com>
2021-06-27 20:22:06 +1200

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,3 @@ app = Flask(__name__)
 def home():
     api_key=os.environ['GOOGLE_CALENDAR_API_KEY']
     return render_template("index.html", api_key=api_key)
-
-if __name__ == '__main__':
-    app.run()
